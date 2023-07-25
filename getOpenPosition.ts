df5e0d8bb4fddaf4f326cafdd0a7fc8993b31d9a
gnrbrhm <gnrbrhm@gmail.com>
2023-07-25 09:33:54 +0300

    Get Position request added.

---
+++ getOpenPosition.ts
@@ -0,0 +1,9 @@
+
+import { client } from "./utils/constant";
+
+
+
+(async () => {
+	const positions = await	client.getPositions()
+	console.log('Positions',positions.filter(item=>item.symbol == "BTCUSDT"))
+  })();
--- openFutureOrderWithTPSL.ts
+++ openFutureOrderWithTPSL.ts
@@ -1,15 +1,6 @@
-import { USDMClient, NewFuturesOrderParams,MarkPrice } from "binance";
+import { client } from "./utils/constant";
+import { NewFuturesOrderParams } from "binance";
 
-const testnet = true;
-const baseUrl = testnet
-  ? "https://testnet.binancefuture.com" : 
-    "https://api.binance.com";
-const client = new USDMClient({
-  api_key: "6522dba195264b2a747255c4eb0c2a0819347eea7279e9cebf3e4cc68fcd9bbf", //process.env["BINANCE_API_KEY"],
-  api_secret:
-    "e960984f5431757c3c9b1cb63695ed5eeb585dba300f2b1dcee8fa60a5aa7a92", // process.env["BINANCE_API_SECRET"],
-  baseUrl,
-});
 const symbol = 'BTCUSDT';
 
 // submit a 1:1 bracket market buy order with market entry order
