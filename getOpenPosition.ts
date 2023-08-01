45079248726120e0fa7a15ade0b8517654e24942
birolkudos <birolgoktepe@kudos.is>
2023-08-01 14:52:07 +0300

    console.log added 2.

--- getOpenPosition.ts
+++ getOpenPosition.ts
@@ -4,5 +4,5 @@ import { client } from "./utils/constant";
 
 (async () => {
 	const positions = await	client.getPositions()
-	console.log('Positions Birol Test',positions.filter(item=>item.symbol == "BTCUSDT"))
+	console.log('Positions Birol Test 2',positions.filter(item=>item.symbol == "BTCUSDT"))
   })();
