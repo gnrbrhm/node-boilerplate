d7d226d02d1dd59ced1bfd16f95c58356f82a47c
birolkudos <birolgoktepe@kudos.is>
2023-08-01 14:40:50 +0300

    HG-001 Console.log after Test added 2

--- getOpenPosition.ts
+++ getOpenPosition.ts
@@ -4,9 +4,5 @@ import { client } from "./utils/constant";
 
 (async () => {
 	const positions = await	client.getPositions()
-<<<<<<< HEAD
-	console.log('Positions Deneme Halil Test',positions.filter(item=>item.symbol == "BTCUSDT"))
-=======
-	console.log('Positions Deneme Halil',positions.filter(item=>item.symbol == "BTCUSDT"))
->>>>>>> f7b1462f262241e4f88210ba55c97f3369afa3c1
+	console.log('Positions Deneme Halil Test 2',positions.filter(item=>item.symbol == "BTCUSDT"))
   })();
